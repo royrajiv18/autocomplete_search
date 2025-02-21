1b20452f3161f80a7e30f0592e388e2a6017f27d
Rajiv Roy <rajivpune18@gmail.com>
2025-02-21 11:58:55 +0530

    searchbar added and fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+
+  // fetch data
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/recipes/search?q=");
+    const json = await data.json();
+    console.log(json);
+    setResults(json?.recipes);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [input]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Autocomplete Search Bar</h1>
+      <div>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <div>
+          {results.map((item) => (
+            <span key={item.id}>{item.name}</span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
